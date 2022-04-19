8298e9acc0c0b0b212473e9391405b3c468003cd
Levi Mastrud <levikeithmastrud@gmail.com>
2022-04-18 21:56:27 -0500

    created and tested showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,29 @@
 console.log('***** Music Collection *****')
+let collection = [];
+function addToCollection (title, artist, yearPublished) {
+let added = {
+    title: title,
+    artist: artist,
+    yearPublished, yearPublished
+}
+collection.push(added);
+return added;
+}
+
+console.log(addToCollection('In The Aeroplane Over The Sea', 'Neutral Milk Hotel', '1998'));
+console.log(addToCollection('Yeezus', 'Kanye West', '2013'));
+console.log(addToCollection('The Life Of Pablo', 'Kanye West', '2016'));
+console.log(addToCollection(`I'm Wide Awake, It's Morning`, 'Bright Eyes', '2005'));
+console.log(addToCollection('Love Is Overtaking Me', 'Arthur Russell', '2004'));
+console.log(addToCollection('Common As Light And Love Are Red Valleys Of Blood', 'Sun Kil Moon', '2017'));
+
+console.log(collection);
+
+function showCollection ( array ) {
+    console.log(array.length);
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i].title + ' by ' + array[i].artist + ', published in ' + array[i].yearPublished)
+    }
+}
+
+console.log(showCollection(collection));
